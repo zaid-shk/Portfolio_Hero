3c9f0349eb936d9d982174f5a57905d390e9224e
LOL <mohammadzaid8178@gmail.com>
2025-08-12 16:41:00 +0530

    First Look

---
+++ script.js
@@ -0,0 +1,72 @@
+var tl = gsap.timeline()
+
+tl.from("#nav #home",{
+    y: -50,
+    opacity: 0,
+    duration: 0.4,
+    scrub:1,
+    stagger: 0.3
+})
+tl.from("#nav #about",{
+    y: 50,
+    opacity: 0,
+    duration: 0.4,
+    scrub:1,
+    stagger: 0.3
+})
+tl.from("#nav #services",{
+    y: -50,
+    opacity: 0,
+    duration: 0.4,
+    scrub:1,
+    stagger: 0.3
+})
+tl.from("#nav #portfolio",{
+    y: 50,
+    opacity: 0,
+    duration: 0.4,
+    scrub:1,
+    stagger: 0.3
+})
+tl.from("#nav #contact",{
+    y: -50,
+    opacity: 0,
+    duration: 0.4,
+    scrub:1,
+    stagger: 0.3
+})
+tl.from("#nav #hire-me",{
+    y: 50,
+    opacity: 0,
+    duration: 0.4,
+    scrub:1,
+    stagger: 0.3
+})
+tl.from("#page1 p",{
+    x: 500,
+    opacity: 0,
+    duration: 0.9,
+    scrub:1,
+    stagger: 0.3
+})
+tl.from("#page1 .indx1",{
+    x: -50,
+    opacity: 0,
+    duration: 0.9,
+    scrub:1,
+    stagger: 0.3
+})
+tl.from("#page1 #indx2",{
+    x: -50,
+    opacity: 0,
+    duration: 0.9,
+    scrub:1,
+    stagger: 0.3
+})
+tl.from("#page1 img",{
+    x: -500,
+    opacity: 0,
+    duration: 0.9,
+    scrub:1,
+    stagger: 0.3
+})
